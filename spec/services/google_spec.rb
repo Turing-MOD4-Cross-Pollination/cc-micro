e9b59c5ddf8c80e864d0087e4a9f59a6d63b6bdd
StarPerfect <StarPerfect@gmail.com>
2020-01-08 11:06:31 -0700

    Create Google Service Spec
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

---
+++ spec/services/google_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'google geocoding api service' do
+    it 'returns lat and long of a given location' do
+        location = 'Denver, CO'
+        service = GoogleService.new(location)
+
+        expect(service).to be_an_instance_of(Google)
+    end 
+end 
